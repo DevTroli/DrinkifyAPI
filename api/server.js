671168d38b13637f757634bdd087dca1107ed904
Troli <brawlixo123@gmail.com>
2023-12-22 22:14:28 -0300

    new update!

--- api/server.js
+++ api/server.js
@@ -2,15 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const express = require('express');
 const path = require('path');
 
 server.use(middlewares);
-server.use('/images/*', express.static(path.join(__dirname, 'images')));
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
   '/images/*': '/images/$1',
-  '/blog/:resource/:id/show': '/:resource/:id'
 }));
 server.use(router);
 server.listen(3000, () => {
